06cd7b8d3cffa841a34a7d5fafa182e1f5af07a9
Jared <jmorgan4257@gmail.com>
2017-08-12 10:51:21 -0500

    pushing some java

---
+++ js/budget.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	// Start stuff here!!
+	$("#add-income").click(function() {
+		$("#test1").append("<div class='input-group'><input type='text' class='form-control' aria-describedby='basic-addon1'></div>");
+		$("#test2").append("<div class='input-group'><input type='text' class='form-control' aria-describedby='basic-addon1'></div>");
+	});
+	$("#add-expense").click(function() {
+
+	});
+
+});
