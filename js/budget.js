0759e21e13ee2a4747f6b603e341f600d8ea0612
Jared <jmorgan4257@gmail.com>
2017-08-12 11:31:03 -0500

    add income row testing

--- js/budget.js
+++ js/budget.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 	// Start stuff here!!
 	$("#add-income").click(function() {
-		$("#test1").append("<div class='input-group'><input type='text' class='form-control' aria-describedby='basic-addon1'></div>");
-		$("#test2").append("<div class='input-group'><input type='text' class='form-control' aria-describedby='basic-addon1'></div>");
+		$("#test1").append("<div class='input-group'><input type='text' class='form-control' aria-describedby='basic-addon1' placeholder='Add an Income Name'></div><br>");
+		$("#test2").append("<div class='input-group'><input type='text' class='form-control' aria-describedby='basic-addon1' placeholder='Add an Income Amount'></div><br>");
 	});
 	$("#add-expense").click(function() {
 
