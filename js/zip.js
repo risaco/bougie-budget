26229180e9a3ac2799508d3712eb597141cc63d8
Meg O'Roark <msoroark@gmail.com>
2017-08-14 18:21:46 -0500

    keys and call for zipcode API

--- js/zip.js
+++ js/zip.js
@@ -11,21 +11,48 @@ $(document).ready(function() {
 
   firebase.initializeApp(config);
   var database = firebase.database();
+
   //Submit zip
-  $('#zip-submit').on('click', function() {
+  $('#zip-submit').on('click', function(event) {
     event.preventDefault();
     //grab zip
     var zipcode = $('#zip-input').val().trim();
-    console.log(zipcode);
-    var pushedZip = {
-      zip: zipcode
-    }
-    //Push to firebase
-    database.ref().push(pushedZip);
-    //Empty field
-    $('#zip-input').val("");
 
-    location.href = "budget.html";
+
+    //call API to find state of zip-input
+    var cKey = "js-tEvHMQxhKGkoJDfTyw24kpbAamNPl3HPIcxu34MNK7buKrnjXqnhPzS0VFPhJw7r";
+    var key = "LMVzZkjQUtY8fT1Ik33NM2tUjGxG0RSMEOlE9Fu49scome6Hhau3dYYwFwdGn5s2";
+    var queryURL = "https://www.zipcodeapi.com/rest/" + key + "/info.json/" + zipcode + "/degrees";
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .done(function(response) {
+        var results = response.data;
+
+        console.log(results);
+        //Firebase object
+        var locData = {
+          zip: zipcode,
+
+        }
+
+        //Push to firebase
+        database.ref().push(locData);
+        //Empty field
+        $('#zip-input').val("");
+
+      })
+
+
+
+
+
+
+    // location.href = "budget.html";
   });
+
 
 });
