2218eacfbc82d6c098c8c89017db22d188a54f3f
Meg O'Roark <msoroark@gmail.com>
2017-08-14 20:13:30 -0500

    input 1st page, call for loc

--- js/budget.js
+++ js/budget.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
+  //Input zip code-Page 1
 
+  //Submit zip
+  $('#zip-submit').on('click', function(event) {
+    event.preventDefault();
+    //grab zip
+    var zipcode = $('#zip-input').val().trim();
+
+    //call API to find state of zip-input
+    var key = "AIzaSyAaK5LW94pb1MJ1T8qZ_am1xvXEdwEuC0w";
+    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode + "&key=" + key;;
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .done(function(response) {
+        console.log(response);
+        var results = response.results[0].address_components;
+        var city = results[1].long_name;
+        var stateAbv = results[4].short_name;
+
+        $('#zip-input').val("");
+
+        location.href = "budget.html";
+      })
+  });
   // Adding income button function.
   $("#add-income").click(function() {
     $("#income-block").append("<div class='row repeating-income-row'> <div class='col-md-6'> <div class='input-group'> <input type='text' class='form-control' id='income-label-input' aria-describedby='basic-addon1' placeholder='Type your custom input label here.'> </div> <br> </div> <div class='col-md-6'> <div class='input-group'> <input type='text' class='form-control' id='income-amount-input' aria-describedby='basic-addon1' placeholder='1000'> </div> </div> </div>");
@@ -8,5 +35,5 @@ $(document).ready(function() {
   $("#add-expense").click(function() {
 
   });
-
+  //Buttons
 });
